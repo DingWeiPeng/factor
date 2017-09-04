4e4462cba45fa92d86c463144f1949a4c700b99b
Graham Hemsley <ghemsley@protonmail.ch>
2017-09-04 18:41:33 -0500

    Added factor.py
    factor.py: finds all factors of a positive integer passed to it as an argument on the command line

---
+++ factor.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def factor(__n):
+
+    __factorList = []
+
+    for __i in range(1,__n+1):
+        if __n % __i == 0:
+            __factorList.append(__i)
+    return __factorList
+
+def main():
+
+    import argparse
+    from sys import exit
+
+    __parser = None
+    __args = None
+
+    __parser = argparse.ArgumentParser()
+    __parser.add_argument("number", help="Number to factor", type=int)
+    __args = __parser.parse_args()
+    print(str(factor(__args.number)))
+    exit()
+
+main()
