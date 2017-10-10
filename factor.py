ea334679e272d9f6e875d2a831db01ba13777d77
Graham Hemsley <ghemsley@protonmail.ch>
2017-10-10 13:13:14 -0500

    Added support for negative numbers and 0

--- factor.py
+++ factor.py
@@ -4,9 +4,12 @@ def factor(__n):
 
     __factorList = []
 
-    for __i in range(1,__n+1):
-        if __n % __i == 0:
-            __factorList.append(__i)
+    if __n > 0 or __n < 0:
+        for __i in range(1,abs(int(__n)+1)):
+            if __n % __i == 0:
+                __factorList.append(__i)
+    elif __n == 0:
+        __factorList.append("Undefined")
     return __factorList
 
 def main():
@@ -18,7 +21,7 @@ def main():
     __args = None
 
     __parser = argparse.ArgumentParser()
-    __parser.add_argument("number", help="Number to factor", type=int)
+    __parser.add_argument("number", help="Number to factor", type=float)
     __args = __parser.parse_args()
     print(str(factor(__args.number)))
     exit()
